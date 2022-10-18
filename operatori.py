58c90e6c39dcf6881ddc2249764b267e7d4e18e4
Anna Alexa Dezsi <104918814+AnnaDezsi@users.noreply.github.com>
2022-10-18 15:09:37 +0300

    creare algorimti

---
+++ operatori.py
@@ -0,0 +1,54 @@
+def suma():
+
+    print("Suma")
+    print("Introduceti primul numar de la tastatura (a)")
+    a = input()
+    print("a=", a)
+    print("Introduceti al doilea1 numar de la tastatura (b)")
+    b = input()
+    print("b=", b)
+    print("a+b=", int(a) + int(b))
+
+
+def scadere():
+    print("Scadere")
+    print("Introduceti primul numar de la tastatura (a)")
+    a = input()
+    print("a=", a)
+    print("Introduceti al doilea1 numar de la tastatura (b)")
+    b = input()
+    print("b=", b)
+    print("a-b=", int(a) - int(b))
+
+def inmultire():
+    print("Inmultire")
+    print("Introduceti primul numar de la tastatura (a)")
+    a = input()
+    print("a=", a)
+    print("Introduceti al doilea1 numar de la tastatura (b)")
+    b = input()
+    print("b=", b)
+    print("a-b=", int(a) * int(b))
+
+def impartire():
+    print("Impartire")
+    print("Introduceti primul numar de la tastatura (a)")
+    a = input()
+    print("a=", a)
+    print("Introduceti al doilea1 numar de la tastatura (b)")
+    b = input()
+    print("b=", b)
+    print("a-b=", int(a) / int(b))
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    suma()
+    scadere()
+    inmultire()
+    impartire()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
